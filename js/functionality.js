88e04f6775940b72ddf93c097388c572e6fab6aa
Jordan Carr <JordanCarr42@gmail.com>
2017-04-08 20:10:03 -0600

    JS functionality improvement and feature addition and, html and css adjustments

--- js/functionality.js
+++ js/functionality.js
@@ -7,44 +7,55 @@ var valueToBeat = 0;
 var playerTurn = 1;
 
 $(document).ready(function () {
-    // This is only a click event where you can't get the values from form elements
-    $("#submitButton").click(function () {
-        $("#PlayerOneNameDisplay").html($("#playerOneName").val());
-        $("#PlayerTwoNameDisplay").html($("#playerTwoName").val());
-    });
     // This is a form submission where you can get all the values from the form elements
     $("form.newGameForm").submit(function (e) {
-        // $("#ID").val()
-        $("#PlayerOneNameDisplay").html($("#playerOneName").val());
-        $("#PlayerTwoNameDisplay").html($("#playerTwoName").val());
-
-        // Calling a post REST API to the card values when you click new game
-        $.post("http://ins.mtroyal.ca/~nkhemka/test/process.php").done(function (data) {
-            // convert POST response to a JAVASCRIPT OBJECT/variable
-            var deck = $.parseJSON(data);
-            valueToBeat = deck.Cards[0].value;
-            // putting all 10 card values from POST into the ARRAY- this is our game memory
-            for (var i = 0; i < myCardValues.length; i++) {
-                myCardValues[i] = deck.Cards[i].value;
-            }
-            // we are creating events for each card -- the way you will identify what card you have clicked
-            for (var cardIndex = 0; cardIndex < myCardValues.length; cardIndex++) {
-                var card = $("#Card" + cardIndex);
-                card.html(myCardValues[cardIndex]);
-                card.click(function (e) {
-                    var cardIndex = e.target.id.substr(4, 1);
-                    var card = $(e.target);
-                    selectCard(cardIndex, card);
-                    console.log("You clicked card: " + cardIndex);
-                    console.log("This card has a value of: " + myCardValues[cardIndex])
-                });
-            }
-        });
-        $("#Move").html(valueToBeat);
-        $(".cardArea").css("visibility", "visible");
+        loadPlayers();
+        loadCards();
+        setupEndGame();
         e.preventDefault();
     });
 });
+
+function loadPlayers() {
+    $("#PlayerOneNameDisplay").html($("#playerOneName").val());
+    $("#PlayerTwoNameDisplay").html($("#playerTwoName").val());
+
+    switch (playerTurn) {
+        case 1:
+            $("#PlayerMove").html("Player 1, value to beat: ");
+            break;
+        case 2:
+            $("#PlayerMove").html("Player 2, value to beat: ")
+    }
+
+    $("#Move").html(valueToBeat);
+}
+
+function loadCards() {
+    $.post("http://ins.mtroyal.ca/~nkhemka/test/process.php").done(function (data) {
+        // convert POST response to a JAVASCRIPT OBJECT/variable
+        var deck = $.parseJSON(data);
+        valueToBeat = deck.Cards[0].value;
+        // putting all 10 card values from POST into the ARRAY- this is our game memory
+        for (var i = 0; i < myCardValues.length; i++) {
+            myCardValues[i] = deck.Cards[i + 1].value;
+        }
+        // we are creating events for each card -- the way you will identify what card you have clicked
+        for (var cardIndex = 0; cardIndex < myCardValues.length; cardIndex++) {
+            var card = $("#Card" + cardIndex);
+            card.html(myCardValues[cardIndex]);
+            card.click(function (e) {
+                var cardIndex = e.target.id.substr(4, 1);
+                var card = $(e.target);
+                selectCard(cardIndex, card);
+                console.log("You clicked card: " + cardIndex);
+                console.log("This card has a value of: " + myCardValues[cardIndex])
+            });
+        }
+    });
+
+    $(".cardArea").css("visibility", "visible");
+}
 
 function selectCard(cardIndex, card) {
     // Store Card Selected Value
@@ -68,4 +79,10 @@ function selectCard(cardIndex, card) {
             card.css("background-color", ""); //un-sets colour
             break;
     }
+}
+
+function setupEndGame() {
+    $("#EndGameButton").click(function () {
+        window.location.reload();
+    });
 }
