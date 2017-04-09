261e2a88b1fcfbf6b8b1f4ab61da7879820d03f6
Jordan Carr <JordanCarr42@gmail.com>
2017-04-08 23:40:07 -0600

    added transpile directory

--- dist/js/functionality.js
+++ dist/js/functionality.js
@@ -1,10 +1,12 @@
+"use strict";
+
 /**
  * Created by Jordan Carr on 2017-03-28.
  */
-let myCardValues = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let selectedCards = [false, false, false, false, false, false, false, false, false, false];
-let valueToBeat = 0;
-let playerTurnValue = 0;
+var myCardValues = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var selectedCards = [false, false, false, false, false, false, false, false, false, false];
+var valueToBeat = 0;
+var playerTurnValue = 0;
 
 $(document).ready(function () {
 
@@ -29,22 +31,22 @@ function loadPlayers() {
 }
 
 function playerTurn(playerNumber) {
-    const playerMoveElement = $("#PlayerMove");
-    playerMoveElement.html(`Player ${playerNumber}, value to beat: `);
+    var playerMoveElement = $("#PlayerMove");
+    playerMoveElement.html("Player " + playerNumber + ", value to beat: ");
 }
 
 function loadCards() {
     $.post("http://ins.mtroyal.ca/~nkhemka/test/process.php").done(function (data) {
         // convert POST response to a JAVASCRIPT OBJECT/variable
-        const deck = $.parseJSON(data);
+        var deck = $.parseJSON(data);
         valueToBeat = deck.Cards[0].value;
         // putting all 10 card values from POST into the ARRAY- this is our game memory
-        for (let i = 0; i < myCardValues.length; i++) {
+        for (var i = 0; i < myCardValues.length; i++) {
             myCardValues[i] = deck.Cards[i + 1].value;
         }
         // we are creating events for each card -- the way you will identify what card you have clicked
-        for (let cardIndex = 0; cardIndex < myCardValues.length; cardIndex++) {
-            const card = $("#Card" + cardIndex);
+        for (var cardIndex = 0; cardIndex < myCardValues.length; cardIndex++) {
+            var card = $("#Card" + cardIndex);
             card.html(myCardValues[cardIndex]);
             card.click(cardSetup);
         }
@@ -54,11 +56,11 @@ function loadCards() {
 }
 
 function cardSetup(e) {
-    const cardIndex = e.target.id.substr(4, 1);
-    const card = $(e.target);
+    var cardIndex = e.target.id.substr(4, 1);
+    var card = $(e.target);
     selectCard(cardIndex, card);
     console.log("You clicked card: " + cardIndex);
-    console.log("This card has a value of: " + myCardValues[cardIndex])
+    console.log("This card has a value of: " + myCardValues[cardIndex]);
 }
 
 function selectCard(cardIndex, card) {
@@ -76,10 +78,12 @@ function selectCard(cardIndex, card) {
     switch (card.css("background-color")) {
         // blue = #0288D1, rgb(2, 136, 209)
         // red  = #F44336, rgb(244, 67, 54)
-        case "rgb(2, 136, 209)": // blue
+        case "rgb(2, 136, 209)":
+            // blue
             card.css("background-color", "#F44336"); // red
             break;
-        case "rgb(244, 67, 54)": // red
+        case "rgb(244, 67, 54)":
+            // red
             card.css("background-color", ""); //un-sets colour
             break;
     }
@@ -90,3 +94,4 @@ function setupEndGame() {
         window.location.reload();
     });
 }
+//# sourceMappingURL=functionality.js.map
